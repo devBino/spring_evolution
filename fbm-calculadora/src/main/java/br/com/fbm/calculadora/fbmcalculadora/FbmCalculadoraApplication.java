30dc6899b7ee254bab7b4f7b1c28d4f737eae745
Fernando Bino Machado <programemachado@gmail.com>
2023-01-03 08:10:20 -0300

    Creating a simple calc and validating customized exceptions

---
+++ fbm-calculadora/src/main/java/br/com/fbm/calculadora/fbmcalculadora/FbmCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fbm.calculadora.fbmcalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbmCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbmCalculadoraApplication.class, args);
+	}
+
+}
---
+++ fbm-calculadora/src/main/java/br/com/fbm/calculadora/fbmcalculadora/controllers/CalcController.java
@@ -0,0 +1,47 @@
+package br.com.fbm.calculadora.fbmcalculadora.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fbm.calculadora.fbmcalculadora.exception.UnsupportedCalcException;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class CalcController {
+    
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+        method=RequestMethod.GET)
+    public Double sum(
+        @PathVariable(value="numberOne") String pNumberOne,
+        @PathVariable(value="numberTwo") String pNumberTwo)
+            throws UnsupportedCalcException {
+
+        if(!isNumeric(pNumberOne) || !isNumeric(pNumberTwo)){
+            throw new UnsupportedCalcException("Please set a numeric value");
+        }
+
+        return convertToDouble(pNumberOne)  + convertToDouble(pNumberTwo);
+
+    }
+
+    private boolean isNumeric(String pNumber){
+        if(pNumber == null) return false;
+        String convertNumber = pNumber.replace(",", ".");
+        return convertNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String pNumber){
+        
+        if(pNumber == null) return 0D;
+
+        String convertNumber = pNumber.replace(",", ".");
+
+        if(isNumeric(convertNumber)) return Double.parseDouble(convertNumber);
+
+        return 0D;
+
+    }
+
+}
